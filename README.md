2189c8b21642ab71183881f5749fe442636f1de6
marawan gamal <marawan.gamal@live.com>
2021-07-18 18:21:13 -0400

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # tracking
+
+### Usage
+
+1. Install Deformable Convolution Networks package (dcn_v2)
+```commandline
+git clone https://github.com/CharlesShang/DCNv2.git
+```
+```commandline
+cd DCNv2 && ./make.sh  # this adds it to PYTHONPATH
+```
+
+2. Download hockey pre-trained model and put it "src/models"
+    - Go to https://www.dropbox.com/s/r8v8lvt87fm8cjm/TRACING-and-TEAMid.zip?dl=0
+    - Download model found at tracking/models/hockey_20.pth
+
+3. Run tracking algo
+```commandline
+python tracking/src/run_tracking.py mot --videos_dir /home/marawan/data/hockey_videos \
+--load_model tracking/models/hockey_20.pth --conf_thres 0.5 --output-format video \
+--output-root /home/marawan/data/hockey_videos_tracked
+```
--- src/run_tracking.py
+++ src/run_tracking.py
@@ -1,22 +1,3 @@
-"""
-Usage:
-
-    1. Install Deformable Convolution Networks package (dcn_v2)
-        git clone https://github.com/CharlesShang/DCNv2.git
-        cd DCNv2 && ./make.sh  # this adds it to PYTHONPATH
-
-    2. Download hockey pre-trained model and put it "src/models"
-        - go to https://www.dropbox.com/s/r8v8lvt87fm8cjm/TRACING-and-TEAMid.zip?dl=0
-        - download model found at tracking/models/hockey_20.pth
-
-    3. Run tracking algo
-        python tracking/src/run_tracking.py mot --videos_dir /home/marawan/data/hockey_videos \
-        --load_model tracking/models/hockey_20.pth --conf_thres 0.5 --output-format video \
-        --output-root data/videos_tracked
-"""
-
-
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
